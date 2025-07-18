a29e59956803b919d416d4eb8b2b4e57017a8a04
caroldmg <carolinadmg98@gmail.com>
2025-07-18 18:11:13 +0200

    Actualizada inicialización de estructuras

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cde-migu <marvin@42.fr>                    (  V  ) (  V  )  .        */
 /*                                                 /--m-m- /--m-m-    +       */
 /*   Created: 2025/07/18 15:30:23 by cde-migu                      *    .     */
-/*   Updated: 2025/07/18 17:23:57 by cde-migu       tortolitas       .        */
+/*   Updated: 2025/07/18 18:07:23 by cde-migu       tortolitas       .        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,27 @@ t_game	*init_game(t_gctrl *gctrl)
 	game = gctrl_malloc(gctrl, PROG_BLOCK, sizeof(t_game));
 	game.map.grid = gctrl_malloc(gctrl, PROG_BLOCK, 32 * sizeof(char *));
 
-	int fd = open("maps/dev.cub", O_RDONLY);
-	for (int i = 0; i < 32; i++)
-		map[i] = get_next_line(fd);
+	{
+		int fd = open("maps/dev.cub", O_RDONLY);
+		for (int i = 0; i < 32; i++)
+			game.map.grid[i] = get_next_line(fd);
+		game.map.height = 32;
+		game.map.width = 32;
+		game.map.floor_color = 0xB00B69;
+		game.map.ceiling_color = 0x69B00B;
+		close (fd);
+	}
+
+	game.player.x = 16;
+	game.player.y = 16;
+	game.player.dir = 0;
+
+	game.mlx = mlx_init();
+	game.win = mlx_new_window(game.mlx, WIN_WIDTH, WIN_HEIGHT, TITLE);
+
+	game.base_img.img = mlx_new_image(game.mlx, WIN_WIDTH, WIN_HEIGHT);
+
+	game.base_img.img_addr = mlx_get_data_addr(game.base_img.img, &game.base_img.bpp, &game.base_img.l_len, &game.base_img.endian);
 
 	return (game);
 }
