315658d1f29da9d182db615ea9925cd914cd0f6b
luna <luna7111@tuta.io>
2025-07-19 17:17:28 +0200

    Fixed norme stuff

--- src/main.c
+++ src/main.c
@@ -12,17 +12,16 @@
 
 #include <cub3d.h>
 
-void    init_graphics(t_game *game)
+void	init_graphics(t_game *game)
 {
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, WIN_WIDTH, WIN_HEIGHT, TITLE);
 	game->base_img.img = mlx_new_image(game->mlx, WIN_WIDTH, WIN_HEIGHT);
 	game->base_img.addr = mlx_get_data_addr(game->base_img.img,
-		&game->base_img.bpp, &game->base_img.l_len, &game->base_img.endian);
-	
-	/* yo haría una función para inicializar una textura y pondría cuatro */
-	/* llamadas a esa función dentro de esta :) */ 
+			&game->base_img.bpp, &game->base_img.l_len, &game->base_img.endian);
 }
+/* yo haría una función para inicializar una textura y pondría cuatro */
+/* llamadas a esa función dentro de esta :) */
 
 t_game	*init_game(t_gctrl *gctrl)
 {
@@ -30,9 +29,8 @@ t_game	*init_game(t_gctrl *gctrl)
 
 	game = gctrl_malloc(gctrl, PROG_BLOCK, sizeof(t_game));
 	game->map.grid = gctrl_malloc(gctrl, PROG_BLOCK, 32 * sizeof(char *));
-
 	{
-		int fd = open("maps/dev.cub", O_RDONLY);
+		int	fd = open("maps/dev.cub", O_RDONLY);
 		for (int i = 0; i < 32; i++)
 			game->map.grid[i] = get_next_line(fd);
 		game->map.height = 32;
@@ -41,11 +39,9 @@ t_game	*init_game(t_gctrl *gctrl)
 		game->map.ceiling_color = 0x69B00B;
 		close (fd);
 	}
-
 	game->player.x = 16;
 	game->player.y = 16;
 	game->player.dir = 0;
-
 	init_graphics(game);
 	/* he movido estas cosas a la función init_graphics */
 	/* game->mlx = mlx_init(); */
@@ -53,9 +49,9 @@ t_game	*init_game(t_gctrl *gctrl)
 	/* game->base_img.img = mlx_new_image(game->mlx, WIN_WIDTH, WIN_HEIGHT); */
 	/* game->base_img.addr = mlx_get_data_addr(game->base_img.img, */
 	/*	&game->base_img.bpp, &game->base_img.l_len, &game->base_img.endian); */
-
 	return (game);
 }
+/* habrá que actualizarla con las cosas del parseo bien :) */
 
 int main(void)
 {
@@ -64,35 +60,34 @@ int main(void)
 
 	gctrl = gctrl_init();
 	game = init_game(gctrl);
-	/* habrá que actualizarla con las cosas del parseo bien :) */
 	while (1)
 		;
-    (void)game;
+	(void)game;
 }
-/*                     ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ */                        
-/*                   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ */                      
-/*                   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ */                      
-/*                   ▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒ */                      
-/*                   ▒▒▒▒▒▒  ░░  ▒▒▒▒▒▒▒▒ */                      
-/*                   ▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒ */                      
-/*                   ▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒ */                      
-/*                     ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ */                        
-/*                   ▓▓▒▒▓▓▒▒▒▒▒▒▒▒▓▓░░ */                        
-/*                 ▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓░░ */                      
-/*                 ▓▓▓▓▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒░░░░ */                    
-/*                 ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░░░░ */                  
-/*               ░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░▒▒░░░░ */                
-/*             ░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░▒▒▒▒▒▒▒▒ */              
-/*             ▒▒▒▒▒▒▓▓▓▓▓▓▓▓▒▒▓▓▒▒▒▒░░░░▒▒▒▒▒▒▒▒ */              
-/*             ▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▓▓▓▓▒▒ */            
-/*               ▒▒░░░░░░░░░░░░░░░░░░░░░░░░▓▓▒▒▒▒▓▓ */            
-/*                 ░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▓▓▒▒ */            
-/*                 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒░░ */            
-/*                   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */              
-/*                       ░░░░░░░░░░░░░░░░░░░░░░░░ */              
+/*                     ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ */
+/*                   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ */
+/*                   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ */
+/*                   ▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒ */
+/*                   ▒▒▒▒▒▒  ░░  ▒▒▒▒▒▒▒▒ */
+/*                   ▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒ */
+/*                   ▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒ */
+/*                     ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ */
+/*                   ▓▓▒▒▓▓▒▒▒▒▒▒▒▒▓▓░░ */
+/*                 ▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓░░ */
+/*                 ▓▓▓▓▒▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒░░░░ */
+/*                 ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░░░░ */
+/*               ░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░▒▒░░░░ */
+/*             ░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░▒▒▒▒▒▒▒▒ */
+/*             ▒▒▒▒▒▒▓▓▓▓▓▓▓▓▒▒▓▓▒▒▒▒░░░░▒▒▒▒▒▒▒▒ */
+/*             ▒▒▒▒░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▓▓▓▓▒▒ */
+/*               ▒▒░░░░░░░░░░░░░░░░░░░░░░░░▓▓▒▒▒▒▓▓ */
+/*                 ░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▓▓▒▒ */
+/*                 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒░░ */
+/*                   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
+/*                       ░░░░░░░░░░░░░░░░░░░░░░░░ */
 /* ▒▒▓▓▒▒▓▓▒▒▓▓▒▒▓▓▒▒▓▓░░▓▓▒▒▓▓▒▒▓▓░░▓▓▒▒▓▓▒▒▓▓▒▒▓▓▒▒▓▓▓▓▒▒▓▓▒▒ */
-/*                       ░░          ░░░░▒▒░░▒▒░░ */              
-/*                                     ░░▒▒░░▒▒░░ */              
-/*                                     ░░▒▒░░▒▒░░ */              
-/*                                     ░░▒▒░░▒▒░░ */              
-/*                                       ▒▒░░▒▒ */                
+/*                       ░░          ░░░░▒▒░░▒▒░░ */
+/*                                     ░░▒▒░░▒▒░░ */
+/*                                     ░░▒▒░░▒▒░░ */
+/*                                     ░░▒▒░░▒▒░░ */
+/*                                       ▒▒░░▒▒ */
